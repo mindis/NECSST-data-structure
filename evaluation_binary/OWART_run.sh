1ba01f02a6f4557e29958dc51dbedc371620d298
SeKwonLee <sekwonlee90@gmail.com>
2016-08-08 12:33:46 +0900

    20160808

--- evaluation_binary/OWART_run.sh
+++ evaluation_binary/OWART_run.sh
@@ -1,20 +1,58 @@
 #!/bin/sh
 
 #dense workload
-./OWART_dense_16M_4 > ./result/OWART_dense_16M_4.txt
-./OWART_dense_16M_5 > ./result/OWART_dense_16M_5.txt
-./OWART_dense_16M_6 > ./result/OWART_dense_16M_6.txt
-./OWART_dense_16M_7 > ./result/OWART_dense_16M_7.txt
-./OWART_dense_16M_8 > ./result/OWART_dense_16M_8.txt
+#./OWART_dense_16M_4 > ./result/OWART_dense_16M_4.txt
+#./OWART_dense_16M_5 > ./result/OWART_dense_16M_5.txt
+#./OWART_dense_16M_6 > ./result/OWART_dense_16M_6.txt
+#./OWART_dense_16M_7 > ./result/OWART_dense_16M_7.txt
+#./OWART_dense_16M_8 > ./result/OWART_dense_16M_8.txt
 
-./OWART_dense_128M_4 > ./result/OWART_dense_128M_4.txt
-./OWART_dense_128M_5 > ./result/OWART_dense_128M_5.txt
-./OWART_dense_128M_6 > ./result/OWART_dense_128M_6.txt
-./OWART_dense_128M_7 > ./result/OWART_dense_128M_7.txt
-./OWART_dense_128M_8 > ./result/OWART_dense_128M_8.txt
+#./OWART_dense_128M_4 > ./result/OWART_dense_128M_4.txt
+#./OWART_dense_128M_5 > ./result/OWART_dense_128M_5.txt
+#./OWART_dense_128M_6 > ./result/OWART_dense_128M_6.txt
+#./OWART_dense_128M_7 > ./result/OWART_dense_128M_7.txt
+#./OWART_dense_128M_8 > ./result/OWART_dense_128M_8.txt
 
-./OWART_dense_1024M_4 > ./result/OWART_dense_1024M_4.txt
-./OWART_dense_1024M_5 > ./result/OWART_dense_1024M_5.txt
-./OWART_dense_1024M_6 > ./result/OWART_dense_1024M_6.txt
-./OWART_dense_1024M_7 > ./result/OWART_dense_1024M_7.txt
-./OWART_dense_1024M_8 > ./result/OWART_dense_1024M_8.txt
+#./OWART_dense_1024M_4 > ./result/OWART_dense_1024M_4.txt
+#./OWART_dense_1024M_5 > ./result/OWART_dense_1024M_5.txt
+#./OWART_dense_1024M_6 > ./result/OWART_dense_1024M_6.txt
+#./OWART_dense_1024M_7 > ./result/OWART_dense_1024M_7.txt
+#./OWART_dense_1024M_8 > ./result/OWART_dense_1024M_8.txt
+
+#sparse workload
+#./OWART_sparse_16M_4 > ./result/OWART_sparse_16M_4.txt
+#./OWART_sparse_16M_5 > ./result/OWART_sparse_16M_5.txt
+#./OWART_sparse_16M_6 > ./result/OWART_sparse_16M_6.txt
+#./OWART_sparse_16M_7 > ./result/OWART_sparse_16M_7.txt
+#./OWART_sparse_16M_8 > ./result/OWART_sparse_16M_8.txt
+
+#./OWART_sparse_128M_4 > ./result/OWART_sparse_128M_4.txt
+#./OWART_sparse_128M_5 > ./result/OWART_sparse_128M_5.txt
+#./OWART_sparse_128M_6 > ./result/OWART_sparse_128M_6.txt
+#./OWART_sparse_128M_7 > ./result/OWART_sparse_128M_7.txt
+#./OWART_sparse_128M_8 > ./result/OWART_sparse_128M_8.txt
+
+#./OWART_sparse_1024M_4 > ./result/OWART_sparse_1024M_4.txt
+#./OWART_sparse_1024M_5 > ./result/OWART_sparse_1024M_5.txt
+#./OWART_sparse_1024M_6 > ./result/OWART_sparse_1024M_6.txt
+#./OWART_sparse_1024M_7 > ./result/OWART_sparse_1024M_7.txt
+#./OWART_sparse_1024M_8 > ./result/OWART_sparse_1024M_8.txt
+
+#WART dense workload
+./WART_dense_16M_4 > ./result/WART_dense_16M_4.txt
+./WART_dense_16M_5 > ./result/WART_dense_16M_5.txt
+./WART_dense_16M_6 > ./result/WART_dense_16M_6.txt
+./WART_dense_16M_7 > ./result/WART_dense_16M_7.txt
+./WART_dense_16M_8 > ./result/WART_dense_16M_8.txt
+
+./WART_dense_128M_4 > ./result/WART_dense_128M_4.txt
+./WART_dense_128M_5 > ./result/WART_dense_128M_5.txt
+./WART_dense_128M_6 > ./result/WART_dense_128M_6.txt
+./WART_dense_128M_7 > ./result/WART_dense_128M_7.txt
+./WART_dense_128M_8 > ./result/WART_dense_128M_8.txt
+
+./WART_dense_1024M_4 > ./result/WART_dense_1024M_4.txt
+./WART_dense_1024M_5 > ./result/WART_dense_1024M_5.txt
+./WART_dense_1024M_6 > ./result/WART_dense_1024M_6.txt
+./WART_dense_1024M_7 > ./result/WART_dense_1024M_7.txt
+./WART_dense_1024M_8 > ./result/WART_dense_1024M_8.txt
